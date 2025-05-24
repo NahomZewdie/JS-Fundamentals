4701d15018d69bb938265efbba978c19f1f70154
Nahom Zewdie <zewdienahom@gmail.com>
2025-05-24 18:16:31 +0300

    corrections

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,12 +1,13 @@
-function printer (print){
-    if (!print){
-       console.log("No argument");}
-    else {
-        console.log("Argument found");
-    }
-    
-    }
-   
-printer();
-printer("Best");
-printer("Best school");
+const numberOfArguments = process.argv.length - 2; // Subtract 2 to exclude 'node' and the script file path.
+
+// Check the number of arguments and print the appropriate message.
+if (numberOfArguments === 0) {
+  // If no arguments are passed by the user
+  console.log("No argument");
+} else if (numberOfArguments === 1) {
+  // If exactly one argument is passed by the user
+  console.log("Argument found");
+} else {
+  // If more than one argument is passed by the user
+  console.log("Arguments found");
+}
