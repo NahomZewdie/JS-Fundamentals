598ee9dc209a2b677f3b335487b381505e1bb354
Nahom Zewdie <zewdienahom@gmail.com>
2025-05-24 18:22:12 +0300

    corrections

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,13 +1,8 @@
-const numberOfArguments = process.argv.length - 2; // Subtract 2 to exclude 'node' and the script file path.
-
-// Check the number of arguments and print the appropriate message.
+const numberOfArguments = process.argv.length - 2; 
 if (numberOfArguments === 0) {
-  // If no arguments are passed by the user
   console.log("No argument");
 } else if (numberOfArguments === 1) {
-  // If exactly one argument is passed by the user
   console.log("Argument found");
 } else {
-  // If more than one argument is passed by the user
   console.log("Arguments found");
 }
