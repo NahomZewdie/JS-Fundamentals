5519cba6886986e5aa8956b346ec916e0fcb2bc6
NahomZewdie <zewdienahom@gmail.com>
2025-05-25 13:50:35 +0300

    spaces

--- 8-square.js
+++ 8-square.js
@@ -2,11 +2,11 @@ let x = Number(process.argv[2]);
 let string="X"
 let i=0;
 if (Number.isInteger(x)) {
-    while(i<x){
+    while (i<x){
         console.log(string.repeat(x));
         i++;
     }
 }
-else{
+else {
     console.log("Missing size");
 }
