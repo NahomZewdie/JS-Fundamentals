1135a2a18b6660a41ac44069a0bb33cdd34f4818
NahomZewdie <zewdienahom@gmail.com>
2025-05-25 13:17:06 +0300

    changed it to integer first

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,9 @@
-let x = process.argv[2];
-let i=0;
-if(isNaN(x)){{
-    console.log("Missing number of occurrences");
-}}
-else{
-    while(i<=x){
-        console.log("C is fun");
-        i++;
-    }
+const x = Number(process.argv[2]);
+
+if (Number.isInteger(x)) {
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
+} else {
+  console.log("Missing number of occurrences");
 }
